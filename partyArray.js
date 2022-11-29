c6028ac3ddcc21e6f5c05f733612444a2842977f
Albruce Appesh <albruceappesh@gmail.com>
2022-11-29 07:04:35 -0500

    first commit

---
+++ partyArray.js
@@ -0,0 +1,34 @@
+let party = [
+    {
+        name: 'Joline',
+        hitpoints: 15,
+        belongings: ["spear", "bread", "Tums"],
+        companion: {
+            name: "Tabby",
+            type: "cat",
+        }
+    },
+    {
+        name: 'Timothy',
+        hitpoints: 10,
+        belongings: ["sword", "potion", "Tums"],
+        companion: {
+            name: "Velma",
+            type: "Bat",
+        }
+    },
+    {
+        name: 'Sarah',
+        hitpoints: 20,
+        belongings: ["bow", "arrows", "wood"],
+        companion: {
+            name: "Tony",
+            type: "tiger",
+        }
+    },
+]
+
+// double the hitpoints of everyone in the party 
+// loop through party
+// double hitpoints and reassign 
+
